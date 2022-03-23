1405a8a898caffc1c63d2f221428b702ff342cb0
rushieshpatil20 <rushipatil20032003@gmail.com>
2022-03-23 09:02:12 +0530

    ADDED:[rushiesh] program for Addition of 2 Number

---
+++ ADD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("Enter any 2 Number");
+	scanf("%d%d",&c);
+	printf("\n Addtion=%d",c);
+	c=a+b;
+	getch();
+}
