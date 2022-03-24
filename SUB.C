82d347e0df3c9dec4cff14d57ee883a71ec13465
rushieshpatil20 <rushipatil20032003@gmail.com>
2022-03-24 09:08:40 +0530

    ADDED:[rushiesh]program for Subtraction of 2 Number

---
+++ SUB.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr()
+	printf("\nEnter any 2 number");
+	scanf("%d %d",&a,&b);
+	c=a-b;
+	printf("\nSbtraction=%d",c);
+	getch()
+}
+
+
+
