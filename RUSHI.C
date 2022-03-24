55fde716cbdc9497630903ea2213b94b257145cd
rushieshpatil20 <rushipatil20032003@gmail.com>
2022-03-24 09:40:51 +0530

    ADDED:[rushiesh]program for champaring for useing if-else

---
+++ RUSHI.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	int a,b;
+	printf("\nENTER THE VALUE OF A ");
+	scanf("%d", a);
+	printf("\n ENTER THE VALUE OF B");
+	scanf("%d",&b);
+	if(a>b)
+	{
+		printf("in A is big ");
+
+	}
+	else
+	{
+		printf("In B is big ");
+	}
+	}
+
+
+
+
+	getch();
